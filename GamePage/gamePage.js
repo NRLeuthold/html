1a3753fcca5e31c265b03cf090311c3d997981e6
NoahL <nrleuthold@gmail.com>
2022-07-14 09:54:03 -0500

    Fix colorGameExplain dynamic width JS

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -18,7 +18,7 @@ function alignGamePage() {
     colorGame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorGameSTYLE.width))/2 + 'px';
 
     colorGameExplainSTYLE = getComputedStyle(colorGameExplain);
-    colorGameExplain.style.width = parseInt(colorGameSTYLE.width) - (parseInt(colorImgSTYLE.width) + 20) + "px";
+    colorGameExplain.style.width = parseInt(colorGameSTYLE.width) - (parseInt(colorImgSTYLE.width) + 30) + "px";
     colorGameExplain.style.left = parseInt(colorGameSTYLE.width) - parseInt(colorGameExplainSTYLE.width) - 10 + "px";
 }
 
