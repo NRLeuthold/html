38146b83d42683d0afc7e334ea8d3f4cad0ffad6
NoahL <nrleuthold@gmail.com>
2022-07-14 07:56:48 -0500

    changed 'main.js' to 'Universal Files/header.js'

--- Universal Files/header.js
+++ Universal Files/header.js
@@ -1,16 +1,10 @@
 const header = document.getElementById("header");
 const logo = document.getElementById("logo");
-const mainContent = document.getElementById("mainContent");
 
-function refreshing(){
-    var mainContentSTYLE = getComputedStyle(mainContent);
+function alignHeader(){
     var headerSTYLE = getComputedStyle(header);
-
     var logoSTYLE = getComputedStyle(logo);
 
-
-
-    mainContent.style.left = (window.innerWidth/2) - (parseInt(mainContentSTYLE.width)/2) + "px";
     header.style.left = (window.innerWidth/2) - (parseInt(headerSTYLE.width)/2) + "px";
 
     logo.style.left = (parseInt(headerSTYLE.height)-(parseInt(logoSTYLE.height)))/2 + "px";
@@ -18,5 +12,5 @@ function refreshing(){
 }
 
 var refresh = setInterval(function(){
-    refreshing();
+    alignHeader();
 }, .1);
