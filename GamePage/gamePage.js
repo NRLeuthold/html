35f728af4642ef71c01ade317f12d15add6864dc
NoahL <nrleuthold@gmail.com>
2022-07-14 09:15:48 -0500

    Fix colorGame JS

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -1,16 +1,16 @@
 const title = document.getElementById("title");
 var titleSTYLE = getComputedStyle(title);
 
-const colorgame = document.getElementById("colorgame");
-var colorgameSTYLE = getComputedStyle(colorgame);
+const colorGame = document.getElementById("colorGame");
+var colorGameSTYLE = getComputedStyle(colorGame);
 
 
 function alignGamePage() {
     titleSTYLE = getComputedStyle(title);
     title.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(titleSTYLE.width))/2 + 'px';
 
-    colorgameSTYLE = getComputedStyle(colorgame);
-    colorgame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorgameSTYLE.width))/2 + 'px';
+    colorGameSTYLE = getComputedStyle(colorGame);
+    colorGame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorGameSTYLE.width))/2 + 'px';
 }
 
 var refresh = setInterval(function(){
