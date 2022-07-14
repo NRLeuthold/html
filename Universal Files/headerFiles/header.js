4f2f80ab3a0fb9688eae6054f09c0b26bc97da8a
NoahL <nrleuthold@gmail.com>
2022-07-14 13:48:44 -0500

    Remove buttons div

--- Universal Files/headerFiles/header.js
+++ Universal Files/headerFiles/header.js
@@ -4,9 +4,6 @@ var headerSTYLE = getComputedStyle(header);
 const logo = document.getElementById("logo");
 var logoSTYLE = getComputedStyle(logo);
 
-const buttons = document.getElementById("buttons");
-var buttonsSTYLE = getComputedStyle(buttons);
-
 function alignHeader(){
     headerSTYLE = getComputedStyle(header);
     header.style.left = (window.innerWidth/2) - (parseInt(headerSTYLE.width)/2) + "px";
@@ -14,11 +11,8 @@ function alignHeader(){
     logoSTYLE = getComputedStyle(logo);
     logo.style.left = (parseInt(headerSTYLE.height)-(parseInt(logoSTYLE.height)))/2 + "px";
 
-    
-    buttonsSTYLE = getComputedStyle(buttons);
-    buttons.style.width = (parseInt(headerSTYLE.width)) - (parseInt(logoSTYLE.width)) - (parseInt(headerSTYLE.width * .15)) + "px";
-    buttons.style.left = (parseInt(logoSTYLE.width)) + (parseInt(headerSTYLE.width * .1)) + "px";
 }
+
 
 var refresh = setInterval(function(){
     alignHeader();
