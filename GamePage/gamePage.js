920d792f25591b2dafc66b01ce6002be05a40e08
NoahL <nrleuthold@gmail.com>
2022-07-14 08:52:41 -0500

    Testing JS variable issue

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -4,7 +4,7 @@ var colorGameSTYLE = getComputedStyle(colorGame);
 function alignGamePage() {
     colorGameSTYLE = getComputedStyle(colorGame);
     colorGame.style.left = (parseInt(mainContentSTYLE.width)/2) - parseInt(colorGameSTYLE.width/2) + 'px';
-    console.log(commonContentSTYLE.width);
+    console.log(mainContentSTYLE.width);
 }
 
 var refresh = setInterval(function(){
