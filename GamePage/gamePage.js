5779cd590bf9735eeb22264d99b5a6e8902daed2
NoahL <nrleuthold@gmail.com>
2022-07-14 09:14:28 -0500

    Fix colorGame CSS and add JS

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -1,9 +1,16 @@
 const title = document.getElementById("title");
 var titleSTYLE = getComputedStyle(title);
 
+const colorgame = document.getElementById("colorgame");
+var colorgameSTYLE = getComputedStyle(colorgame);
+
+
 function alignGamePage() {
     titleSTYLE = getComputedStyle(title);
     title.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(titleSTYLE.width))/2 + 'px';
+
+    colorgameSTYLE = getComputedStyle(colorgame);
+    colorgame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorgameSTYLE.width))/2 + 'px';
 }
 
 var refresh = setInterval(function(){
