f12b69d3772f7da9e1d17acf3c61fab35b05f86a
NoahL <nrleuthold@gmail.com>
2022-07-14 13:36:58 -0500

    Added buttons div

--- Universal Files/headerFiles/header.js
+++ Universal Files/headerFiles/header.js
@@ -1,15 +1,22 @@
 const header = document.getElementById("header");
-const logo = document.getElementById("logo");
 var headerSTYLE = getComputedStyle(header);
+
+const logo = document.getElementById("logo");
 var logoSTYLE = getComputedStyle(logo);
+
+const buttons = document.getElementById("buttons");
+var buttonsSTYLE = getComputedStyle(buttons);
 
 function alignHeader(){
     headerSTYLE = getComputedStyle(header);
-    logoSTYLE = getComputedStyle(logo);
-
     header.style.left = (window.innerWidth/2) - (parseInt(headerSTYLE.width)/2) + "px";
+
+    logoSTYLE = getComputedStyle(logo);
     logo.style.left = (parseInt(headerSTYLE.height)-(parseInt(logoSTYLE.height)))/2 + "px";
 
+    
+    buttonsSTYLE = getComputedStyle(buttons);
+    buttons.style.left = (parseInt(logoSTYLE.width)) + (parseInt(headerSTYLE.width * .1)) + "px";
 }
 
 var refresh = setInterval(function(){
