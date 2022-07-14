987d0f6ca38d62309659c5ef3d180293166289f5
NoahL <nrleuthold@gmail.com>
2022-07-14 08:59:45 -0500

    Moving title div

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -1,10 +1,9 @@
-const colorGame = document.getElementById("colorGame");
-var colorGameSTYLE = getComputedStyle(colorGame);
+const title = document.getElementById("title");
+var titleSTYLE = getComputedStyle(title);
 
 function alignGamePage() {
-    colorGameSTYLE = getComputedStyle(colorGame);
-    colorGame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorGameSTYLE.width))/2 + 'px';
-    console.log(mainContentSTYLE.width, "\n", colorGameSTYLE.width);
+    titleSTYLE = getComputedStyle(title);
+    title.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(titleSTYLE.width))/2 + 'px';
 }
 
 var refresh = setInterval(function(){
