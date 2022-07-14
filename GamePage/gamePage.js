8bc1aa3e318b7fde576b672363943d0daa4bbd14
NoahL <nrleuthold@gmail.com>
2022-07-14 08:57:06 -0500

    Attempt 2 fixing JS variable issue

--- GamePage/gamePage.js
+++ GamePage/gamePage.js
@@ -3,7 +3,7 @@ var colorGameSTYLE = getComputedStyle(colorGame);
 
 function alignGamePage() {
     colorGameSTYLE = getComputedStyle(colorGame);
-    colorGame.style.left = (parseInt(mainContentSTYLE.width)/2) - parseInt(colorGameSTYLE.width/2) + 'px';
+    colorGame.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(colorGameSTYLE.width))/2 + 'px';
     console.log(mainContentSTYLE.width, "\n", colorGameSTYLE.width);
 }
 
