7a4396541ae6b5a5babf4c0bab0fac3036f3835b
NoahL <nrleuthold@gmail.com>
2022-07-15 09:10:14 -0500

    Fix capitalization error in APIPage.js

--- APIPage/APIPage.js
+++ APIPage/APIPage.js
@@ -1,7 +1,7 @@
 const title = document.getElementById("title");
 var titleSTYLE = getComputedStyle(title);
 
-function alignAPiPage(){
+function alignAPIPage(){
     
     titleSTYLE = getComputedStyle(title);
     title.style.left = ((parseInt(mainContentSTYLE.width)) - parseInt(titleSTYLE.width))/2 + 'px';
