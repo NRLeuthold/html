78a1f8a74e8ce6d243971cb64ab471cdff00ab7e
NoahL <nrleuthold@gmail.com>
2022-07-16 07:38:43 -0500

    Trying to get PW to work

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var request = new XMLHttpRequest();
 
-request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+request.open("GET", "https://passwordwolf.com", true);
 
 request.onload = function () {
     var data = JSON.parse(this.response);
@@ -15,7 +15,7 @@ request.onload = function () {
 request.send();
 
 
-//This script pulls the names from url and adds them to an array and then puts the on the screen on APIPage
+//This script pulls the names from the url and adds them to an array and then puts them on the screen on APIPage
 // TODO: Find an API that meets CORS standards
 // TODO: Replace the current url with new API
 // TODO: Find a way to implement API into APIPage depending on API uses
