71510777c283f56d2671edef813237e3ce50a047
NoahL <nrleuthold@gmail.com>
2022-07-15 21:26:57 -0500

    Adding XHR to APIPage

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 var request = new XMLHttpRequest();
 
+var movies = [];
+
 request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
 
 request.onload = function () {
     var data = JSON.parse(this.response);
 
     data.forEach((movie) => {
-        console.log(movie.title);
+        movies.push(movie.title);
     });
 };
 
 request.send();
+
+APIOutput.textContent = movies;
