90bb2aa63b265b0271323a272b6c34923704baba
NoahL <nrleuthold@gmail.com>
2022-07-15 21:13:57 -0500

    Following XHR article

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest();
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
 
-request.onload = function () {
-  // Begin accessing JSON data here
-}
+request.onload = function () {};
 
-// Send request
-request.send()
+request.send();
 
-console.log("Hello");
+var data = JSON.parse(this.response);
+
+data.forEach((movie) => {
+    console.log(movie.title);
+});
