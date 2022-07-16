a3374b12260fc26dbb1b46d00830fac5231ac790
NoahL <nrleuthold@gmail.com>
2022-07-15 21:36:49 -0500

    Added some comments to test.js

--- test.js
+++ test.js
@@ -13,3 +13,9 @@ request.onload = function () {
 };
 
 request.send();
+
+
+//This script pulls the names from url and adds them to an array and then puts the on the screen on APIPage
+// TODO: Find an API that meets CORS standards
+// TODO: Replace the current url with new API
+// TODO: Find a way to implement API into APIPage depending on API uses
