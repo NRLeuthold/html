fed9e3fb5c3a57f0360e504326e46ae0990ef318
NoahL <nrleuthold@gmail.com>
2022-07-15 21:17:47 -0500

    Fixing test.js

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 var request = new XMLHttpRequest();
 
 request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
 
-request.onload = function () {};
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach((movie) => {
+            console.log(movie.title);
+        });
+    } else {
+        console.log("error");
+    }
+};
 
 request.send();
-
-
-var data = JSON.parse(this.response);
-
-data.forEach((movie) => {
-    console.log(movie.title);
-});
