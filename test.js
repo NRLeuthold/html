a5e2eee5b518a798ab23195ce7c4636a2dfefe25
NoahL <nrleuthold@gmail.com>
2022-07-16 08:08:46 -0500

    Remove XHR code ffrom test.js

--- test.js
+++ test.js
@@ -11,27 +11,9 @@ fetch(apiUrl)
         // Do something for an error here
     });
 
-/*var request = new XMLHttpRequest();
-var url = "https://spotify.com/"
-
-request.open("GET", url);
-
-request.onload = function () {
-    var data = JSON.parse(this.response);
-    var movies = [];
-
-    data.forEach((movie) => {
-        movies.push(movie.title);
-    });
-    APIOutput.textContent = movies;
-};
-
-request.send();
-
-testing
 
 //This script pulls the names from the url and adds them to an array and then puts them on the screen on APIPage
 // TODO: Find an API that meets CORS standards
 // TODO: Replace the current url with new API
 // TODO: Find a way to implement API into APIPage depending on API uses
-*/
+
