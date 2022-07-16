d7ddab0934906c5ca7e43d7fed2a7237acd124c5
NoahL <nrleuthold@gmail.com>
2022-07-16 17:39:23 -0500

    Trying agify.io

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var ageUrl = "https://api.agify.io?name=noah";
 var genderUrl = "https://api.genderize.io?name=noah";
+var nationalityUrl = "https://api.nationalize.io?name=noah";
 fetch(ageUrl)
     .then((response) => {
         return response.json();
@@ -24,7 +25,14 @@ fetch(genderUrl)
         // Do something for an error here
     });
 
-//This script pulls the names from the url and adds them to an array and then puts them on the screen on APIPage
-// TODO: Find an API that meets CORS standards
-// TODO: Replace the current url with new API
-// TODO: Find a way to implement API into APIPage depending on API uses
+fetch(nationalityUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        // Work with JSON data here
+        console.log(data);
+    })
+    .catch((err) => {
+        // Do something for an error here
+    });
