c026b1be471d69fe97f6624eb60c1ec9ad2d83dd
NoahL <nrleuthold@gmail.com>
2022-07-15 20:52:26 -0500

    Change test.js to work with PW

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 import fetch from "http://10.0.0.101/node_modules/node-fetch";
 
-const response = fetch("https://passwordwolf.com/api/");
-
-if (!response.ok) {
-    throw new Error(`Error! status: ${response.status}`);
-}
-
-const result = response.json();
-console.log(result);
+fetch('https://passwordwolfcom/api')
+    .then(res => res.text())
+    .then(text => console.log(text));
 
