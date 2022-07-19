42a6ff0a29eba11c7e3615ed4c9e069e09bbdb00
NoahL <nrleuthold@gmail.com>
2022-07-19 12:52:25 -0500

    Adding simonGame

--- GamePage/GamePage.js
+++ GamePage/GamePage.js
@@ -1,3 +1,6 @@
+const simonGame = document.getElementById("simonGame");
+var simonGameSTYLE = getComputedStyle(simonGame);
+
 const simonImg = document.getElementById("simonImg");
 var simonImgSTYLE = getComputedStyle(simonImg);
 
@@ -8,7 +11,7 @@ function alignGamePage() {
     simonImg.style.width = parseFloat(simonImgSTYLE.height) + "px";
     simonImg.style.left = parseFloat(simonImgSTYLE.top) + "px";
 
-    simonTitle.style.left = parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.right) + "px";
+    simonTitle.style.left = parseFloat(simonImgSTYLE.left) + (parseFloat(simonGame) - (parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width)))/2 + "px";
 
 }
 
