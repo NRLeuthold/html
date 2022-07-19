e99891ce92f778758a662d0b00e9b1ae9528adcf
NoahL <nrleuthold@gmail.com>
2022-07-19 13:26:04 -0500

    Adding simonGame

--- GamePage/GamePage.js
+++ GamePage/GamePage.js
@@ -15,7 +15,7 @@ function alignGamePage() {
     simonImg.style.left = parseFloat(simonImgSTYLE.top) + "px";
 
     simonTitle.style.left = parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width) + (parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width)))/2 - parseFloat(simonTitleSTYLE.width)/2 + "px";
-    simonTitle.style.top = (parseFloat(simonGameSTYLE.height) - (parseFloat(simonTitleSTYLE.height) + parseFloat(simonBlurbSTYLE.height) + 10))/2 + "px";
+    simonTitle.style.top = (parseFloat(simonGameSTYLE.height) - (parseFloat(simonTitleSTYLE.height) + parseFloat(simonBlurbSTYLE.height) - 10))/2 + "px";
 
     simonBlurb.style.width = parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left)*2 + parseFloat(simonImgSTYLE.width))
     simonBlurb.style.left = parseFloat(simonImgSTYLE.left)*2 + parseFloat(simonImgSTYLE.width) + (parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width)))/2 - parseFloat(simonBlurbSTYLE.width)/2 + "px";
