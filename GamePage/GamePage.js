677ac872b2d5c15a5036588ba60c05b3f199e79c
NoahL <nrleuthold@gmail.com>
2022-07-19 13:09:28 -0500

    Adding simonGame

--- GamePage/GamePage.js
+++ GamePage/GamePage.js
@@ -7,11 +7,18 @@ var simonImgSTYLE = getComputedStyle(simonImg);
 const simonTitle = document.getElementById("simonTitle");
 var simonTitleSTYLE = getComputedStyle(simonTitle);
 
+const simonBlurb = document.getElementById("simonBlurb");
+var simonBlurbSTYLE = getComputedStyle(simonBlurb);
+
 function alignGamePage() {
     simonImg.style.width = parseFloat(simonImgSTYLE.height) + "px";
     simonImg.style.left = parseFloat(simonImgSTYLE.top) + "px";
 
     simonTitle.style.left = parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width) + (parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width)))/2 - parseFloat(simonTitleSTYLE.width)/2 + "px";
+    
+    simonBlurb.style.width = parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left)*2 + parseFloat(simonImgSTYLE.width))
+    simonBlurb.style.left = parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width) + (parseFloat(simonGameSTYLE.width) - (parseFloat(simonImgSTYLE.left) + parseFloat(simonImgSTYLE.width)))/2 - parseFloat(simonBlurbSTYLE.width)/2 + "px";
+
 }
 
 var refresh = setInterval(function () {
